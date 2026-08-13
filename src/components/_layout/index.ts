5d09b6830ec1bd9541368747d321e2a86b4e3d28
Julien Julien <contact@julienjulien.fr>
2026-08-13 16:45:03 +0200

    [LRZ-CODEX] 🧱 layout > complete public layout exports > 🤠 Julien

--- src/components/_layout/index.ts
+++ src/components/_layout/index.ts
@@ -1,3 +1,25 @@
 export { default as AmbientPageFrame } from "./AmbientPageFrame";
+export { PageControls } from "./PageControls";
+export { PageFooter } from "./PageFooter";
+export {
+    PageHeader,
+    PageHeaderBreadcrumbs,
+    PageHeaderIndexAvailability,
+    PageHeaderIndexMark,
+    PageHeaderIndexNavigation,
+} from "./PageHeader";
 
 export type { AmbientPageFrameProps } from "./AmbientPageFrame";
+export type {
+    PageControlsFilterGroup,
+    PageControlsFilterOption,
+    PageControlsMode,
+    PageControlsVariant,
+    PageControlsViewGroup,
+} from "./PageControls";
+export type { PageFooterProps } from "./PageFooter";
+export type {
+    PageHeaderBreadcrumbItem,
+    PageHeaderProps,
+    PageHeaderVariant,
+} from "./PageHeader";
